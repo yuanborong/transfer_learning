1fbd4126d42cedb199b6432d9dfba90be7c281e3
yuanborong <akb477@qq.com>
2020-06-17 11:28:20 +0800

    test random sample

--- transfer_learning/code_BR/disease_group_random_sampling_AUC_analysis.py
+++ transfer_learning/code_BR/disease_group_random_sampling_AUC_analysis.py
@@ -11,11 +11,10 @@ for data_num in [1]:
     # training data
     train_ori = pd.read_csv('/home/liukang/Doc/valid_df/train_{}.csv'.format(data_num))
 
+    sample_size = [np.linspace(0.05, 1, 20, endpoint=True)]
+
     # for disease_num in range(disease_list.shape[0]):
     for disease_num in [0]:
-        sample_size = [np.linspace(0.05 , 1 , 20 , endpoint=True)]
-        print(sample_size)
-
         # find patients with a certain disease
         train_feature_true = train_ori.loc[:, disease_list.iloc[disease_num, 0]] > 0
         train_meaningful_sample = train_ori.loc[train_feature_true]
@@ -27,15 +26,24 @@ for data_num in [1]:
         X_test = test_meaningful_sample.drop(['Label'], axis=1)
         y_test = test_meaningful_sample['Label']
 
-        # random sampling for test auc
-        random_sampling_train_meaningful_sample = train_meaningful_sample.sample(frac=0.05,axis=0)
-        X_train = random_sampling_train_meaningful_sample.drop(['Label'] , axis=1)
-        y_train = random_sampling_train_meaningful_sample['Label']
+        sample_size = sample_size[:10]
 
-        # build LR model for random sampling
-        lr_DG_ran_smp = LogisticRegression(n_jobs=-1)
-        lr_DG_ran_smp.fit(X_train , y_train)
-        y_predict = lr_DG_ran_smp.predict(X_test)
-        auc = roc_auc_score(y_test , y_predict)
-        print(auc)
+        for frac in sample_size:
+            auc_list = []
+
+            # random sampling for test auc
+            random_sampling_train_meaningful_sample = train_meaningful_sample.sample(frac=frac, axis=0)
+            X_train = random_sampling_train_meaningful_sample.drop(['Label'], axis=1)
+            y_train = random_sampling_train_meaningful_sample['Label']
+
+            # build LR model for random sampling
+            lr_DG_ran_smp = LogisticRegression(n_jobs=-1)
+            lr_DG_ran_smp.fit(X_train, y_train)
+            y_predict = lr_DG_ran_smp.predict(X_test)
+            auc = roc_auc_score(y_test, y_predict)
+
+            auc_list.append(auc)
+
+        print(len(auc_list))
+        print(auc_list)
 
