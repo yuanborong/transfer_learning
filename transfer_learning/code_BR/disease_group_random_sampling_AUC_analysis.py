bd1718a0cd39577f92682acc3292453114a0ba62
yuanborong <akb477@qq.com>
2020-06-18 18:00:17 +0800

    10 times iterators.

--- transfer_learning/code_BR/disease_group_random_sampling_AUC_analysis.py
+++ transfer_learning/code_BR/disease_group_random_sampling_AUC_analysis.py
@@ -9,24 +9,24 @@ disease_list=pd.read_csv('/home/liukang/Doc/disease_top_20.csv')
 # csv_path
 csv_path = '/home/huxinhou/WorkSpace_BR/transfer_learning/result/'
 
+# 生成不同的随机抽样比例
+sample_size = []
+for i in range(2 , 21):
+    sample_size.append(i * 0.05)
+
+auc_mean_dataframe = pd.DataFrame( np.ones((len(disease_list),len(sample_size)))*0 , index=disease_list.iloc[: , 0] , columns=sample_size)
+
+print(auc_mean_dataframe)
+
 for data_num in range(1 , 6):
     # test data
     test_ori = pd.read_csv('/home/liukang/Doc/valid_df/test_{}.csv'.format(data_num))
     # training data
     train_ori = pd.read_csv('/home/liukang/Doc/valid_df/train_{}.csv'.format(data_num))
 
-    # 生成不同的随机抽样比例
-    sample_size = []
-    for i in range(2 , 21):
-        sample_size.append(i * 0.05)
-
-    # 写入结果文件，题头（data_1 , data_2）
-    # f_reuslt = open(txt_path, 'a+')
-    # f_reuslt.write('data_{}_result'.format(data_num))
-    # f_reuslt.write('\n')
-    # f_reuslt.close()
-
     print('\nBegin data_' + str(data_num) +'.......\n\n')
+
+    # 初始化一个新的auc_dataframe
     auc_dataframe = pd.DataFrame(index=disease_list.iloc[: , 0] , columns=sample_size)
 
     for disease_num in range(disease_list.shape[0]):
@@ -45,15 +45,14 @@ for data_num in range(1 , 6):
 
         # 按不同的sample_size，df.sample进行随机抽样
         for frac in sample_size:
-            # random sampling for test auc
-            random_sampling_train_meaningful_sample = train_meaningful_sample.sample(frac=frac, axis=0)
-            X_train = random_sampling_train_meaningful_sample.drop(['Label'], axis=1)
-            y_train = random_sampling_train_meaningful_sample['Label']
+            for i in range(0, 10):
+                # random sampling for test auc
+                random_sampling_train_meaningful_sample = train_meaningful_sample.sample(frac=frac, axis=0)
+                X_train = random_sampling_train_meaningful_sample.drop(['Label'], axis=1)
+                y_train = random_sampling_train_meaningful_sample['Label']
 
-            auc_list = []
+                auc_list = []
 
-            # 对一个数据集下的一个亚组的某个比例样本集，进行10次的建模取平均
-            for i in range(0 , 10):
                 # build LR model for random sampling
                 lr_DG_ran_smp = LogisticRegression(n_jobs=-1)
                 lr_DG_ran_smp.fit(X_train, y_train)
@@ -62,19 +61,16 @@ for data_num in range(1 , 6):
                 auc_list.append(auc)
 
             auc_dataframe.loc[disease_list.iloc[disease_num, 0] , frac] = round(np.mean(auc_list) , 6)
+            auc_mean_dataframe.loc[disease_list.iloc[disease_num, 0] , frac] += np.mean(auc_list)
+
+    # auc_dataframe['mean_auc'] = np.mean(auc_dataframe.loc[])
 
     csv_name = 'random_sampling_auc_result_data_{}.csv'.format(data_num)
     auc_dataframe.to_csv(csv_path + csv_name)
 
     print('\nFinish data_' + str(data_num) +'.......\n\n')
 
+auc_mean_dataframe = auc_mean_dataframe.apply(lambda x : round(x / 5 , 6))
+
 
 print("Done........")
-
-# 每次写之前都进行读，那么久不会覆盖
-        # auc结果写入文件
-        # f_reuslt = open(txt_path, 'a+')
-        # f_reuslt.write(disease_list.iloc[disease_num, 0] + ': ' )
-        # f_reuslt.write(str(auc_list))
-        # f_reuslt.write('\n')
-        # f_reuslt.close()
