366c1e844003fb785d58f79d121b61e4ced75d94
yuanborong <akb477@qq.com>
2020-07-29 08:47:13 +0800

    efrdfg

--- transfer_learning/code_BR/analysis_reason/small_sample_size/transfer_large_disease_group_to_small_disease_group/GBM/transfer_from_all_large_disease_data_to_small_disease_group(large_group_size_10%).py
+++ transfer_learning/code_BR/analysis_reason/small_sample_size/transfer_large_disease_group_to_small_disease_group/GBM/transfer_from_all_large_disease_data_to_small_disease_group(large_group_size_10%).py
@@ -35,7 +35,7 @@ def get_true_sample(dataframe , large_group_items):
 
 disease_list = pd.read_csv('/home/liukang/Doc/disease_top_20.csv')
 # csv_path
-csv_path = '/home/huxinhou/WorkSpace_BR/transfer_learning/result/analysis_reason/small_sample_size/transfer_from_large_disease_group_to_small_disease_group/GBM/'
+csv_path = '/home/huxinhou/WorkSpace_BR/transfer_learning/result/analysis_reason/small_sample_size/transfer_from_large_disease_group_to_small_disease_group(large_group_size_10%)/GBM/'
 # set data result csv's name
 auc_target_csv_name = 'transfer_from_all_data_to_small_disease_group_mean.csv'
 auc_source_csv_name = 'group_disease_data_by_global_model_with_all_data.csv'
